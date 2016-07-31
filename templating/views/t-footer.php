19879db810021f9eccd7ff7958479334ccd94b7b
fufufukalifu <opik@mail.unpas.ac.id>
2016-07-31 15:40:35 +0700

    fixed slidder

--- templating/views/t-footer.php
+++ templating/views/t-footer.php
@@ -89,10 +89,15 @@
         
         <!-- ini footer -->
         <script type="text/javascript" src="<?=base_url('assets/plugins/magnific/js/jquery.magnific-popup.min.js');?>"></script>
-        
         <script type="text/javascript" src="<?=base_url('assets/plugins/shuffle/js/jquery.shuffle.min.js');?>"></script>
-        
         <script type="text/javascript" src="<?=base_url('assets/plugins/owl/js/owl.carousel.min.js');?>"></script>
-        
         <script type="text/javascript" src="<?=base_url('assets/javascript/pages/frontend/portfolio.js');?>"></script>
+
+        <!-- slider -->
+        <script type="text/javascript" src="<?=base_url('assets/plugins/layerslider/js/greensock.min.js');?>"></script>
+        <script type="text/javascript" src="<?=base_url('assets/plugins/layerslider/js/transitions.min.js');?>"></script>
+        <script type="text/javascript" src="<?=base_url('assets/plugins/layerslider/js/layerslider.min.js');?>"></script>
+        <!-- slider -->
+        <script type="text/javascript" src="<?=base_url('assets/plugins/owl/js/owl.carousel.min.js');?>"></script>
+        <script type="text/javascript" src="<?=base_url('assets/javascript/pages/frontend/home.js');?>"></script>
 </body>
