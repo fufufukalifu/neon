d6552f23d22d8cf85af60080d8b455645c458e82
penaorange <mr.abstrak@gmail.com>
2016-08-09 21:32:06 +0700

    Modul login tambahan fungsi dan redirect

--- login/controllers/Login.php
+++ login/controllers/Login.php
@@ -1,11 +1,12 @@
 <?php
 
-class Login extends CI_Controller {
+class Login extends MX_Controller {
 
     //put your code here
 
     public function __construct() {
         parent::__construct();
+        $this->load->helper('url');
         $this->load->model('mLogin');
     }
 
@@ -16,13 +17,53 @@ class Login extends CI_Controller {
         $this->load->view('templating/t-footer');
     }
 
+    public function user() {
+        $this->load->view('templating/t-header');
+        $this->load->view('templating/t-navbarUser');
+        $this->load->view('vUser.php');
+        $this->load->view('templating/t-footer1');
+    }
+
     //Fungsi untuk login, mengecek username dan password
     public function validasiLogin() {
         $username = htmlspecialchars($this->input->post('username'));
         $password = md5($this->input->post('password'));
-        
-        echo 'cek login';
-        $this->mLogin->checkUser();
+
+        if ($result = $this->mLogin->cekUser($username, $password)) {
+            //variabelSession
+            $sess_array = array();
+            foreach ($result as $row) {
+
+                $hakAkses = $row->hakAkses;
+                //membuat session
+                $sess_array = array(
+                    'id' => $row->id,
+                    'USERNAME' => $row->namaPengguna,
+                    'HAKAKSES' => $row->hakAkses,
+                        // 'login_peserta' => true,
+                );
+                $this->session->set_userdata($sess_array);
+
+                if ($hakAkses == 'admin') {
+                    echo 'admin';
+//                    redirect(site_url('peserta-free'));
+                } elseif ($hakAkses == 'guru') {
+                    echo 'guru';
+//                    redirect(site_url('peserta-berbayar'));
+                } elseif ($hakAkses == 'murid') {
+                    echo 'murid';
+//                    redirect(site_url('peserta-bimbel'));
+                } elseif ($hakAkses == 'user') {
+                    redirect(base_url('index.php/Login/user'));
+                } else {
+                    echo 'tidak ada hak akses';
+                }
+            }
+            return TRUE;
+        } else {
+            echo 'gagal login';
+            return FALSE;
+        }
     }
 
 }
--- login/models/mLogin.php
+++ login/models/mLogin.php
@@ -8,9 +8,23 @@ class mLogin extends CI_Model {
         parent::__construct();
     }
 
-    public function checkUser() {
-        echo 'akses database';
+    public function cekUser($username, $password) {
+//        echo 'akses database';
+        $this->db->select('*');
+        $this->db->from('tb_pengguna');
+        $this->db->where('namaPengguna', $username);
+        $this->db->where('kataSandi', $password);
+        $this->db->limit(1);
+
+        $query = $this->db->get();
+        if ($query->num_rows() == 1) {
+            return $query->result(); //if data is true
+        } else {
+            echo 'tidak ada akun';
+            return false; //if data is wrong
+        }
     }
+
 }
 ?>
 
