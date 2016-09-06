2d73f4239d797a7cbcc279696c77d2a8408f24ef
fufufukalifu <opik@mail.unpas.ac.id>
2016-09-06 11:21:00 +0700

    push

--- upvideo/controllers/Upvideo.php
+++ upvideo/controllers/Upvideo.php
@@ -1,49 +1,48 @@
-<?php 
+<?php
 /**
-* 
-*/
+ *
+ */
 class Upvideo extends MX_Controller
 {
-	
-	function __construct()
-	{
-		parent::__construct();
-        $this->load->model('mUpvideo');
-	}
 
-	public function index() 
-	{
-        $this->load->view('templating/t-header');
-        $this->load->view('vupvideo');
-        $this->load->view('templating/t-footer');
+    function __construct() {
+        parent::__construct();
+        $this->load->model( 'mUpvideo' );
     }
 
-	//menampilkan view form upload
-	public function formupvideo()
-	{
- 		$this->load->view('templating/t-header');
-        $this->load->view('vupvideo');
-        $this->load->view('templating/t-footer');
-	}
+    public function index() {
+        $this->load->view( 'templating/t-header' );
+        $this->load->view( 'guru/v-left-bar' );
+        $this->load->view( 'vupvideo' );
+        $this->load->view( 'templating/t-footer-back' );
+    }
 
-	// fungsi untuk upload video
-	public function upvideo()
-    {
-       echo "masuk upvideokkkkk";//for testing
+    //menampilkan view form upload
+    public function formupvideo() {
+        $this->load->view( 'templating/t-header' );
+        $this->load->view( 'v-banner-guru' );
+        $this->load->view( 'templating/t-header' );
+        $this->load->view( 'vupvideo' );
+        $this->load->view( 'templating/t-footer' );
+    }
+
+    // fungsi untuk upload video
+    public function upvideo() {
+        echo "masuk upvideokkkkk";//for testing
         $config['upload_path']          = './video/';
         $config['allowed_types']        = 'jpeg|gif|jpg|png|mkv|mp4';
         $config['max_size']             = 90000;
-        $this->load->library('upload',$config);
+        $this->load->library( 'upload', $config );
 
-        if (!$this->upload->do_upload('userfile')) {
+        if ( !$this->upload->do_upload( 'userfile' ) ) {
             echo "gagal";
-            $error = array('error'=>$this->upload->display_errors());
-            $this->load->view('beranda/main_view',$error);
+            $error = array( 'error'=>$this->upload->display_errors() );
+            $this->load->view( 'beranda/main_view', $error );
         } else {
             $file_data = $this->upload->data();
             $data['img'] = base_url().'/images/'.$file_data['file_name'];
-            $this->load->view('beranda/success_msg',$data);
+            $this->load->view( 'beranda/success_msg', $data );
         }
     }
 }
- ?>
+?>
