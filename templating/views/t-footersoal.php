b74a5673dc88196df1a055a24b8a3ab8f4197063
penaorange <mr.abstrak@gmail.com>
2016-10-11 09:12:21 +0700

    time up

--- templating/views/t-footersoal.php
+++ templating/views/t-footersoal.php
@@ -26,47 +26,47 @@
 <script src="<?php echo base_url(); ?>assets/js/soal_to.js"></script>
 
 <script>
-    function countdown(minutes, stat) {
-        var seconds = 60;
-        var mins = minutes;
-        var hour = minutes;
-
-        if (getCookie("minutes") && getCookie("seconds") && stat)
-        {
-            var seconds = getCookie("seconds");
-            var mins = getCookie("minutes");
-        }
-
-        function tick() {
-            var counter = document.getElementById("timer");
-            setCookie("minutes", mins, 10)
-            setCookie("seconds", seconds, 10)
-            var current_minutes = mins - 1
-            seconds--;
-            counter.innerHTML =
-                    current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-            //save the time in cookie
-            if (seconds > 0) {
-                setTimeout(tick, 1000);
-            } else {
-                if (mins > 1) {
-                    // countdown(mins-1);   never reach “00″ issue solved:Contributed by Victor Streithorst
-                    setTimeout(function () {
-                        countdown(parseInt(mins) - 1, false);
-                    }, 1000);
-                } else {
-                    alert('Waktu Habis!');
-                    document.getElementById("hasil").submit();
-                    deleteAllCookies('seconds', 'minutes');
-                }
-
-            }
-        }
-        tick();
-    }
+    //    function countdown(minutes, stat) {
+    //        var seconds = 60;
+    //        var mins = minutes;
+    //        var hour = minutes;
+    //
+    //        if (getCookie("minutes") && getCookie("seconds") && stat)
+    //        {
+    //            var seconds = getCookie("seconds");
+    //            var mins = getCookie("minutes");
+    //        }
+    //
+    //        function tick() {
+    //            var counter = document.getElementById("timer");
+    //            setCookie("minutes", mins, 10)
+    //            setCookie("seconds", seconds, 10)
+    //            var current_minutes = mins - 1
+    //            seconds--;
+    //            counter.innerHTML =
+    //                    current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+    //            //save the time in cookie
+    //            if (seconds > 0) {
+    //                setTimeout(tick, 1000);
+    //            } else {
+    //                if (mins > 1) {
+    //                    // countdown(mins-1);   never reach “00″ issue solved:Contributed by Victor Streithorst
+    //                    setTimeout(function () {
+    //                        countdown(parseInt(mins) - 1, false);
+    //                    }, 1000);
+    //                } else {
+    //                    alert('Waktu Habis!');
+    //                    document.getElementById("hasil").submit();
+    //                    deleteAllCookies('seconds', 'minutes');
+    //                }
+    //
+    //            }
+    //        }
+    //        tick();
+    //    }
 
     function countup(hour, min, stat) {
-        var seconds = 0;
+        var seconds = 57;
         var mins = min;
         var hours = hour;
 
@@ -83,22 +83,25 @@
             setCookie("seconds", seconds, 10);
             setCookie("hours", hours, 10);
 
-            var current_minutes = mins + 1;
-            var current_hours = hours + 1;
+//            var current_minutes = mins + 1;
+//            var current_hours = hours + 1;
 
             seconds++;
 
-            counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+            counter.innerHTML = String(hours) + ":" + String(mins) + ":" + (seconds < 10 ? "0" : "") + String(seconds);
 
             //save the time in cookie
             if (seconds < 60) {
                 setTimeout(tick, 1000);
             } else {
-                if (seconds == 60) {
+                if (seconds == 60 && mins == 60) {
+                    setTimeout(function () {
+                         countup(parseInt(hours)+1, parseInt(mins) + 1, false);
+                    }, 1000);
+                }else if(seconds == 60){
                     setTimeout(function () {
                          countup(parseInt(hours), parseInt(mins) + 1, false);
                     }, 1000);
-                   
                 }
 //                if (mins > 1) {
 //                    // countdown(mins-1);   never reach “00″ issue solved:Contributed by Victor Streithorst
@@ -139,7 +142,7 @@
      deleteAllCookies('hours','seconds', 'minutes');
 //        deleteAllCookies();
 //    countdown(0, true);
-    countup(-1,-1, true);
+    countup(0,58,true);
 
 
     function deleteAllCookies(seconds, mins) {
